51b541092d9476dd40aacc1edefec1c66845018c
Jeff Lee <jeffomatic@gmail.com>
2018-06-04 23:48:28 -0700

    project 04: finish Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,62 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Each row has 32 words, and there are 256 rows
+// First row starts at 16384
+// Word after last row is 16384+(32*256) = 24576
+
+(start)
+  // *row = 16384
+  @16384
+  D=A
+  @row
+  M=D
+
+(rowLoop)
+  // if (ram[24576] & 0xFFFF) goto fillOn
+  D=-1
+  @24576
+  D=D&M
+  @fillOn
+  D;JNE
+
+  // else
+  (fillOff)
+    // rowPixels = 0
+    @rowPixels
+    M=0
+
+    // goto fillRow
+    @fillRow
+    0;JMP
+
+  (fillOn)
+    // rowPixels = 0xFFFF
+    @rowPixels
+    M=-1
+
+  (fillRow)
+    // D = rowPixels
+    @rowPixels
+    D=M
+
+    // *row = D
+    @row
+    A=M
+    M=D
+
+    // row = row + 1
+    @row
+    M=M+1
+
+    // if (24576 - row) == 0 goto start
+    D=M
+    @24576
+    D=A-D
+    @start
+    D;JEQ
+
+    // continue row loop
+    @rowLoop
+    D;JMP
