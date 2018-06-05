12e74d6000d0e2fe7b75cafe37731cfa3073796f
Jeff Lee <jeffomatic@gmail.com>
2018-06-04 23:04:06 -0700

    project 04: finish mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// r2 = 0;
+// while (r0 > 0) {
+//   r2 = r2 + r1
+//   r0--;
+// }
+
+(START)
+@R2
+M=0
+
+@R0 // Optimization: this is the last line of the loop too
+(LOOP)
+// if R0 == 0 goto END
+D=M
+@END
+D;JEQ
+
+// R2 += R1
+@R1
+D=M
+@R2
+M=D+M
+
+// R0 -= 1
+@R0
+M=M-1
+
+// Continue
+@LOOP
+0;JMP
+
+(END)
