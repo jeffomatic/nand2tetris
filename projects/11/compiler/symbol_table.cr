c6e71a8d4c74456fa07d01dedebdafa78a0976a0
Jeff Lee <jeffomatic@gmail.com>
2018-07-21 21:56:50 -0700

    project 11: move SymbolTable to its own file

---
+++ projects/11/compiler/symbol_table.cr
@@ -0,0 +1,29 @@
+class SymbolTable
+  def_clone
+
+  def initialize(klass : ASTNode::Class)
+    @decls = {
+      Compiler::VarScope::Local    => ({} of String => Int32),
+      Compiler::VarScope::Argument => ({} of String => Int32),
+      Compiler::VarScope::Field    => ({} of String => Int32),
+      Compiler::VarScope::Static   => ({} of String => Int32),
+    }
+
+    klass.members.each do |member|
+      member.names.each { |n| declare(member.var_scope, n) }
+    end
+  end
+
+  def declare(var_scope : Compiler::VarScope, identifier : String)
+    table = @decls[var_scope]
+    table[identifier] = table.size
+  end
+
+  def resolve(identifier : String) : Tuple(Compiler::VarScope, Int32)
+    @decls.each do |var_scope, offsets|
+      return {var_scope, offsets[identifier]} if offsets.has_key?(identifier)
+    end
+
+    raise "undeclared variable: #{identifier}"
+  end
+end
